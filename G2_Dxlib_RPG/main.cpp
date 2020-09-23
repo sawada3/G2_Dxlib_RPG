94b2461c5dc5262fcc11b675405332338ce4ebc1
Sawamdean3 <hj-g19010@sist.ac.jp>
2020-09-23 10:00:03 +0900

    マップ作製

---
+++ G2_Dxlib_RPG/main.cpp
@@ -0,0 +1,34 @@
+#include "DxLib.h"
+
+#define GAME_WIDTH			640	//画面の横の大きさ
+#define GAME_HEIGHT			480	//画面の縦の大きさ
+#define GAME_COLOR			32	//画面のカラービット
+
+#define GAME_WINDOW_BAR		0	//タイトルバーはデフォルトにする
+#define GAME_WINDOW_NAME	"Dxlib_Movie"	//ウィンドウのタイトル
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	SetOutApplicationLogValidFlag(FALSE);				//log.txtを出力しない
+	ChangeWindowMode(TRUE);								//ウィンドウモードに設定
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//指定の数値でウィンドウを表示する
+	SetWindowStyleMode(GAME_WINDOW_BAR);				//タイトルバーはデフォルトにする
+	SetMainWindowText(TEXT(GAME_WINDOW_NAME));			//ウィンドウのタイトルの文字
+	SetAlwaysRunFlag(TRUE);								//非アクティブでも実行する
+
+	if (DxLib_Init() == -1) { return -1; }	//ＤＸライブラリ初期化処理
+
+	//無限ループ
+	while (TRUE)
+	{
+		if (ProcessMessage() != 0) { break; }	//メッセージ処理の結果がエラーのとき、強制終了
+		if (ClearDrawScreen() != 0) { break; }	//画面を消去できなかったとき、強制終了
+
+		DrawString(0, 0, "動画を再生しています・・・", GetColor(255, 255, 255));
+
+	}
+
+	DxLib_End();	//ＤＸライブラリ使用の終了処理
+
+	return 0;
+}
